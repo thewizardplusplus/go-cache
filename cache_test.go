3fb25a72a23e662097c2e5470013999fa501c96d
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-03 23:00:00 +0300

    Add a base of tests for the cache.Cache.Set() method

--- cache_test.go
+++ cache_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	hashmap "github.com/thewizardplusplus/go-hashmap"
 )
 
 func TestNewCache(test *testing.T) {
@@ -16,6 +17,33 @@ func TestNewCache(test *testing.T) {
 	mock.AssertExpectationsForObjects(test, storage)
 	assert.Equal(test, storage, cache.storage)
 	assert.Equal(test, getPointer(time.Now), getPointer(cache.clock))
+}
+
+func TestCache_Set(test *testing.T) {
+	type fields struct {
+		storage Storage
+		clock   Clock
+	}
+	type args struct {
+		key  hashmap.Key
+		data interface{}
+		ttl  time.Duration
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			cache := Cache{data.fields.storage, data.fields.clock}
+			cache.Set(data.args.key, data.args.data, data.args.ttl)
+
+			mock.AssertExpectationsForObjects(test, data.fields.storage, data.args.key)
+		})
+	}
 }
 
 func getPointer(value interface{}) uintptr {
