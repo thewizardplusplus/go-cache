89f251bbf6b8396891e72f2e9e84909570b386b6
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-03 23:00:00 +0300

    Generate a mock for the hashmap.Key interface

---
+++ mock_gen.go
@@ -0,0 +1,14 @@
+package cache
+
+import (
+	hashmap "github.com/thewizardplusplus/go-hashmap"
+)
+
+//go:generate mockery -name=Key -inpkg -case=underscore -testonly
+
+// Key ...
+//
+// It's used only for mock generating.
+type Key interface {
+	hashmap.Key
+}
