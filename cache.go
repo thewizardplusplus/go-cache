d5f9976608052cdef583e475439127253545a2cc
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-04 23:00:00 +0300

    Add the cache.Cache.Get() method

--- cache.go
+++ cache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"errors"
 	"time"
 
 	hashmap "github.com/thewizardplusplus/go-hashmap"
@@ -29,9 +30,31 @@ type value struct {
 	expirationTime time.Time
 }
 
+// ...
+var (
+	ErrKeyMissed  = errors.New("key missed")
+	ErrKeyExpired = errors.New("key expired")
+)
+
 // NewCache ...
 func NewCache(storage Storage, clock Clock) Cache {
 	return Cache{storage, clock}
+}
+
+// Get ...
+func (cache Cache) Get(key hashmap.Key) (data interface{}, err error) {
+	data, ok := cache.storage.Get(key)
+	if !ok {
+		return nil, ErrKeyMissed
+	}
+
+	value := data.(value) // nolint: vetshadow
+	expirationTime := value.expirationTime
+	if !expirationTime.IsZero() && cache.clock().After(expirationTime) {
+		return nil, ErrKeyExpired
+	}
+
+	return value.data, nil
 }
 
 // Set ...
