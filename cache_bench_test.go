748bce654b1b8d36a9fff78c479e102711be54a9
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-05 23:00:00 +0300

    Add benchmarks

---
+++ cache_bench_test.go
@@ -0,0 +1,88 @@
+package cache
+
+import (
+	"encoding/binary"
+	"hash/fnv"
+	"math/rand"
+	"testing"
+	"time"
+
+	hashmap "github.com/thewizardplusplus/go-hashmap"
+)
+
+type IntKey int
+
+func (key IntKey) Hash() int {
+	hash := fnv.New32()
+	binary.Write(hash, binary.LittleEndian, int32(key)) // nolint: errcheck
+
+	return int(hash.Sum32())
+}
+
+func (key IntKey) Equals(other interface{}) bool {
+	return key == other.(IntKey)
+}
+
+const (
+	sizeForBench = 1000
+)
+
+func BenchmarkCacheGetting(benchmark *testing.B) {
+	for _, data := range []struct {
+		name      string
+		prepare   func(cache Cache)
+		benchmark func(cache Cache)
+	}{
+		{
+			name: "Get",
+			prepare: func(cache Cache) {
+				for i := 0; i < sizeForBench; i++ {
+					setItem(cache, i)
+				}
+			},
+			benchmark: func(cache Cache) {
+				cache.Get(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
+			},
+		},
+		{
+			name: "GetWithGC",
+			prepare: func(cache Cache) {
+				for i := 0; i < sizeForBench; i++ {
+					setItem(cache, i)
+				}
+			},
+			benchmark: func(cache Cache) {
+				cache.GetWithGC(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
+			},
+		},
+	} {
+		benchmark.Run(data.name, func(benchmark *testing.B) {
+			storage := hashmap.NewConcurrentHashMap()
+			cache := NewCache(storage, time.Now)
+			data.prepare(cache)
+
+			// add concurrent load
+			go func() {
+				for {
+					setItem(cache, rand.Intn(sizeForBench))
+				}
+			}()
+
+			benchmark.ResetTimer()
+
+			for i := 0; i < benchmark.N; i++ {
+				data.benchmark(cache)
+			}
+		})
+	}
+}
+
+func setItem(cache Cache, key int) {
+	var ttl time.Duration
+	// half of items will be already expired
+	if rand.Float32() < 0.5 {
+		ttl = -time.Second
+	}
+
+	cache.Set(IntKey(key), key, ttl)
+}
