b02a84b44cbfc8f085f0f2977af5d0696f926d3f
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-08 23:00:00 +0300

    Add a base of tests for the gc.TotalGC.Clean() method

--- gc/total_gc_test.go
+++ gc/total_gc_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	cache "github.com/thewizardplusplus/go-cache"
 )
 
 func TestNewTotalGC(test *testing.T) {
@@ -17,6 +18,28 @@ func TestNewTotalGC(test *testing.T) {
 	assert.Equal(test, time.Second, gc.period)
 	assert.Equal(test, storage, gc.storage)
 	assert.Equal(test, getPointer(time.Now), getPointer(gc.clock))
+}
+
+func TestTotalGC_Clean(test *testing.T) {
+	type fields struct {
+		period  time.Duration
+		storage Storage
+		clock   cache.Clock
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			gc := TotalGC{data.fields.period, data.fields.storage, data.fields.clock}
+			gc.Clean()
+
+			mock.AssertExpectationsForObjects(test, data.fields.storage)
+		})
+	}
 }
 
 func getPointer(value interface{}) uintptr {
