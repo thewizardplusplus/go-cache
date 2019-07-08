a6dc237bca3b487f0bd1b11c0892f0ddb73264fd
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-08 23:00:00 +0300

    Implement tests for the gc.TotalGC.Clean() method

--- gc/total_gc_test.go
+++ gc/total_gc_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	cache "github.com/thewizardplusplus/go-cache"
+	hashmap "github.com/thewizardplusplus/go-hashmap"
 )
 
 func TestNewTotalGC(test *testing.T) {
@@ -31,7 +32,68 @@ func TestTotalGC_Clean(test *testing.T) {
 		name   string
 		fields fields
 	}{
-		// TODO: add test cases
+		{
+			name: "without values",
+			fields: fields{
+				period: time.Second,
+				storage: func() Storage {
+					storage := new(MockStorage)
+					storage.
+						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
+							return handler != nil
+						})).
+						Return(true)
+
+					return storage
+				}(),
+				clock: clock,
+			},
+		},
+		{
+			name: "with value and its expiration time less than the current one",
+			fields: fields{
+				period: time.Second,
+				storage: func() Storage {
+					storage := new(MockStorage)
+					storage.
+						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
+							return handler != nil
+						})).
+						Return(func(handler hashmap.Handler) bool {
+							return handler(new(MockKey), cache.Value{
+								Data:           "data",
+								ExpirationTime: clock().Add(-time.Second),
+							})
+						})
+					storage.On("Delete", new(MockKey))
+
+					return storage
+				}(),
+				clock: clock,
+			},
+		},
+		{
+			name: "with value and its expiration time greater than the current one",
+			fields: fields{
+				period: time.Second,
+				storage: func() Storage {
+					storage := new(MockStorage)
+					storage.
+						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
+							return handler != nil
+						})).
+						Return(func(handler hashmap.Handler) bool {
+							return handler(new(MockKey), cache.Value{
+								Data:           "data",
+								ExpirationTime: clock().Add(time.Second),
+							})
+						})
+
+					return storage
+				}(),
+				clock: clock,
+			},
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			gc := TotalGC{data.fields.period, data.fields.storage, data.fields.clock}
@@ -44,4 +106,13 @@ func TestTotalGC_Clean(test *testing.T) {
 
 func getPointer(value interface{}) uintptr {
 	return reflect.ValueOf(value).Pointer()
+}
+
+func clock() time.Time {
+	return time.Date(
+		2006, time.January, 2, // year, month, day
+		15, 4, 5, // hour, minute, second
+		0,        // nanosecond
+		time.UTC, // location
+	)
 }
