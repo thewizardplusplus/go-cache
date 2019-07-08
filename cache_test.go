31d524f98c5ebe701280bf1d8c3ba7e9d41604f2
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-08 23:00:00 +0300

    Add a base of tests for the cache.Value.IsExpired() method

--- cache_test.go
+++ cache_test.go
@@ -20,6 +20,32 @@ func NewMockKeyWithID(id int) *MockKeyWithID {
 	return &MockKeyWithID{ID: id}
 }
 
+func TestValue_IsExpired(test *testing.T) {
+	type fields struct {
+		Data           interface{}
+		ExpirationTime time.Time
+	}
+	type args struct {
+		clock Clock
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+		want   assert.BoolAssertionFunc
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			value := Value{data.fields.Data, data.fields.ExpirationTime}
+			got := value.IsExpired(data.args.clock)
+
+			data.want(test, got)
+		})
+	}
+}
+
 func TestNewCache(test *testing.T) {
 	storage := new(MockStorage)
 	cache := NewCache(storage, time.Now)
