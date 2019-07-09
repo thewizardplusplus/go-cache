c8a486ccff361e516889fdaba72903f84641032d
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-09 23:00:00 +0300

    Add the gc.Run() function

--- gc/run.go
+++ gc/run.go
@@ -1,6 +1,26 @@
 package gc
 
+import (
+	"context"
+	"time"
+)
+
 // GC ...
 type GC interface {
 	Clean()
+}
+
+// Run ...
+func Run(ctx context.Context, gc GC, period time.Duration) {
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			gc.Clean()
+		case <-ctx.Done():
+			return
+		}
+	}
 }
