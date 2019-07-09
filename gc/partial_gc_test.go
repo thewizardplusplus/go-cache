cad87fba4a868fa10fdcfa2369b4a7d4fff63ce1
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-09 23:00:00 +0300

    Add a base of tests for the gc.PartialGC.Clean() method

--- gc/partial_gc_test.go
+++ gc/partial_gc_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	cache "github.com/thewizardplusplus/go-cache"
 )
 
 func TestNewPartialGC(test *testing.T) {
@@ -15,4 +16,25 @@ func TestNewPartialGC(test *testing.T) {
 	mock.AssertExpectationsForObjects(test, storage)
 	assert.Equal(test, storage, gc.storage)
 	assert.Equal(test, getPointer(time.Now), getPointer(gc.clock))
+}
+
+func TestPartialGC_Clean(test *testing.T) {
+	type fields struct {
+		storage Storage
+		clock   cache.Clock
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			gc := PartialGC{data.fields.storage, data.fields.clock}
+			gc.Clean()
+
+			mock.AssertExpectationsForObjects(test, data.fields.storage)
+		})
+	}
 }
