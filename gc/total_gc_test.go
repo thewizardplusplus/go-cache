e749328de15ef81e3130be3711f92e858aa79518
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-23 23:00:00 +0300

    Simplify tests of the gc.TotalGC.Clean() method

--- gc/total_gc_test.go
+++ gc/total_gc_test.go
@@ -31,82 +31,19 @@ func TestNewTotalGC(test *testing.T) {
 }
 
 func TestTotalGC_Clean(test *testing.T) {
-	type fields struct {
-		storage Storage
-		clock   cache.Clock
-	}
+	gc := TotalGC{nil, clock}
+	gc.storage = new(MockStorage)
+	gc.storage.(*MockStorage).
+		On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
+			want := reflect.ValueOf(gc.handleIteration).Pointer()
+			got := reflect.ValueOf(handler).Pointer()
+			return want == got
+		})).
+		Return(true)
 
-	for _, data := range []struct {
-		name   string
-		fields fields
-	}{
-		{
-			name: "without values",
-			fields: fields{
-				storage: func() Storage {
-					storage := new(MockStorage)
-					storage.
-						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
-							return handler != nil
-						})).
-						Return(true)
+	gc.Clean()
 
-					return storage
-				}(),
-				clock: clock,
-			},
-		},
-		{
-			name: "with value and its expiration time less than the current one",
-			fields: fields{
-				storage: func() Storage {
-					storage := new(MockStorage)
-					storage.
-						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
-							return handler != nil
-						})).
-						Return(func(handler hashmap.Handler) bool {
-							return handler(NewMockKeyWithID(23), cache.Value{
-								Data:           "data",
-								ExpirationTime: clock().Add(-time.Second),
-							})
-						})
-					storage.On("Delete", NewMockKeyWithID(23))
-
-					return storage
-				}(),
-				clock: clock,
-			},
-		},
-		{
-			name: "with value and its expiration time greater than the current one",
-			fields: fields{
-				storage: func() Storage {
-					storage := new(MockStorage)
-					storage.
-						On("Iterate", mock.MatchedBy(func(handler hashmap.Handler) bool {
-							return handler != nil
-						})).
-						Return(func(handler hashmap.Handler) bool {
-							return handler(NewMockKeyWithID(23), cache.Value{
-								Data:           "data",
-								ExpirationTime: clock().Add(time.Second),
-							})
-						})
-
-					return storage
-				}(),
-				clock: clock,
-			},
-		},
-	} {
-		test.Run(data.name, func(test *testing.T) {
-			gc := TotalGC{data.fields.storage, data.fields.clock}
-			gc.Clean()
-
-			mock.AssertExpectationsForObjects(test, data.fields.storage)
-		})
-	}
+	mock.AssertExpectationsForObjects(test, gc.storage)
 }
 
 func TestTotalGC_handleIteration(test *testing.T) {
