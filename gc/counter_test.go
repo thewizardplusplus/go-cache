e09ba7c725d1a77338a17c6755f573b164b29c52
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-25 23:00:00 +0300

    Add a base of tests for the gc.counter.stopClean() method

--- gc/counter_test.go
+++ gc/counter_test.go
@@ -44,3 +44,28 @@ func Test_counter_stopIterate(test *testing.T) {
 		})
 	}
 }
+
+func Test_counter_stopClean(test *testing.T) {
+	type fields struct {
+		iterated int
+		expired  int
+	}
+
+	for _, data := range []struct {
+		name   string
+		fields fields
+		want   assert.BoolAssertionFunc
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			var counter counter // nolint: vetshadow
+			counter.iterated = data.fields.iterated
+			counter.expired = data.fields.expired
+
+			got := counter.stopClean()
+
+			data.want(test, got)
+		})
+	}
+}
