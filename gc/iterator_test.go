ce81a81b04aed81b33030fc8d5c5e42f515a4a67
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-07-29 23:00:00 +0300

    Add a base of tests for the gc.iterator.handleIteration() method

--- gc/iterator_test.go
+++ gc/iterator_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	cache "github.com/thewizardplusplus/go-cache"
+	hashmap "github.com/thewizardplusplus/go-hashmap"
 )
 
 func Test_newIterator(test *testing.T) {
@@ -16,4 +18,39 @@ func Test_newIterator(test *testing.T) {
 	assert.Zero(test, iterator.counter)
 	assert.Equal(test, storage, iterator.storage)
 	assert.Equal(test, getPointer(time.Now), getPointer(iterator.clock))
+}
+
+func Test_iterator_handleIteration(test *testing.T) {
+	type fields struct {
+		counter counter
+		storage Storage
+		clock   cache.Clock
+	}
+	type args struct {
+		key   hashmap.Key
+		value interface{}
+	}
+
+	for _, data := range []struct {
+		name        string
+		fields      fields
+		args        args
+		wantCounter counter
+		wantOk      assert.BoolAssertionFunc
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			iterator := &iterator{
+				counter: data.fields.counter,
+				storage: data.fields.storage,
+				clock:   data.fields.clock,
+			}
+			gotOk := iterator.handleIteration(data.args.key, data.args.value)
+
+			mock.AssertExpectationsForObjects(test, data.fields.storage, data.args.key)
+			assert.Equal(test, data.wantCounter, iterator.counter)
+			data.wantOk(test, gotOk)
+		})
+	}
 }
