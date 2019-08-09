646254a450134b18b31b65fd508e26d7e3682770
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-08-09 23:00:00 +0300

    Add different storage sizes to benchmarks

--- cache_bench_test.go
+++ cache_bench_test.go
@@ -3,6 +3,7 @@ package cache
 import (
 	"context"
 	"encoding/binary"
+	"fmt"
 	"hash/fnv"
 	"math/rand"
 	"testing"
@@ -25,69 +26,71 @@ func (key IntKey) Equals(other interface{}) bool {
 }
 
 const (
-	sizeForBench   = 1000
 	periodForBench = time.Nanosecond
 	expiredPercent = 0.5
 )
 
 func BenchmarkCacheGetting(benchmark *testing.B) {
-	for _, data := range []struct {
-		name      string
-		prepare   func(cache Cache)
-		benchmark func(cache Cache)
-	}{
-		{
-			name: "Get",
-			prepare: func(cache Cache) {
-				for i := 0; i < sizeForBench; i++ {
-					setItem(cache, i)
-				}
-			},
-			benchmark: func(cache Cache) {
-				cache.Get(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
-			},
-		},
-		{
-			name: "GetWithGC",
-			prepare: func(cache Cache) {
-				for i := 0; i < sizeForBench; i++ {
-					setItem(cache, i)
-				}
-			},
-			benchmark: func(cache Cache) {
-				cache.GetWithGC(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
-			},
-		},
-	} {
-		benchmark.Run(data.name, func(benchmark *testing.B) {
-			storage := hashmap.NewConcurrentHashMap()
-			cache := NewCache(storage, time.Now)
-			data.prepare(cache)
-
-			// add concurrent load
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			go func() {
-				ticker := time.NewTicker(periodForBench)
-				defer ticker.Stop()
-
-				for {
-					select {
-					case <-ticker.C:
-						setItem(cache, rand.Intn(sizeForBench))
-					case <-ctx.Done():
-						return
+	for _, sizeForBench := range []int{1e2, 1e4, 1e6} {
+		for _, data := range []struct {
+			name      string
+			prepare   func(cache Cache)
+			benchmark func(cache Cache)
+		}{
+			{
+				name: "Get",
+				prepare: func(cache Cache) {
+					for i := 0; i < sizeForBench; i++ {
+						setItem(cache, i)
 					}
+				},
+				benchmark: func(cache Cache) {
+					cache.Get(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
+				},
+			},
+			{
+				name: "GetWithGC",
+				prepare: func(cache Cache) {
+					for i := 0; i < sizeForBench; i++ {
+						setItem(cache, i)
+					}
+				},
+				benchmark: func(cache Cache) {
+					cache.GetWithGC(IntKey(rand.Intn(sizeForBench))) // nolint: errcheck
+				},
+			},
+		} {
+			name := fmt.Sprintf("%s/%d", data.name, sizeForBench)
+			benchmark.Run(name, func(benchmark *testing.B) {
+				storage := hashmap.NewConcurrentHashMap()
+				cache := NewCache(storage, time.Now)
+				data.prepare(cache)
+
+				// add concurrent load
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				go func() {
+					ticker := time.NewTicker(periodForBench)
+					defer ticker.Stop()
+
+					for {
+						select {
+						case <-ticker.C:
+							setItem(cache, rand.Intn(sizeForBench))
+						case <-ctx.Done():
+							return
+						}
+					}
+				}()
+
+				benchmark.ResetTimer()
+
+				for i := 0; i < benchmark.N; i++ {
+					data.benchmark(cache)
 				}
-			}()
-
-			benchmark.ResetTimer()
-
-			for i := 0; i < benchmark.N; i++ {
-				data.benchmark(cache)
-			}
-		})
+			})
+		}
 	}
 }
 
