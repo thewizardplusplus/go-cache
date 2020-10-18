76e7a0e0e79cc393dd7dae282403752763ee802d
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-18 23:00:00 +0300

    Add the cache.WithStorage() option

--- option.go
+++ option.go
@@ -1,4 +1,19 @@
 package cache
 
+import (
+	hashmap "github.com/thewizardplusplus/go-hashmap"
+)
+
 // Option ...
 type Option func(cache *Cache)
+
+// WithStorage ...
+//
+// Default: an instance of the hashmap.ConcurrentHashMap structure
+// with default options.
+//
+func WithStorage(storage hashmap.Storage) Option {
+	return func(cache *Cache) {
+		cache.storage = storage
+	}
+}
