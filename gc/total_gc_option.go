84611f43fb8674a6df2d6544547bb856027f1673
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-19 23:00:00 +0300

    Add the gc.TotalGCWithClock() option

--- gc/total_gc_option.go
+++ gc/total_gc_option.go
@@ -1,4 +1,18 @@
 package gc
 
+import (
+	cache "github.com/thewizardplusplus/go-cache"
+)
+
 // TotalGCOption ...
 type TotalGCOption func(gc *TotalGC)
+
+// TotalGCWithClock ...
+//
+// Default: the time.Now() function.
+//
+func TotalGCWithClock(clock cache.Clock) TotalGCOption {
+	return func(gc *TotalGC) {
+		gc.clock = clock
+	}
+}
