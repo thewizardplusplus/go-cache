fa0be77b569961a545942f20309a7737a95c2f56
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-20 23:00:00 +0300

    Fix tests of the gc.counter.stopIterate() method

--- gc/counter_test.go
+++ gc/counter_test.go
@@ -8,8 +8,9 @@ import (
 
 func Test_counter_stopIterate(test *testing.T) {
 	type fields struct {
+		maxIteratedCount int
+
 		iterated int
-		expired  int
 	}
 
 	for _, data := range []struct {
@@ -19,25 +20,26 @@ func Test_counter_stopIterate(test *testing.T) {
 	}{
 		{
 			name:   "without iterations",
-			fields: fields{iterated: 0},
+			fields: fields{maxIteratedCount: 20, iterated: 0},
 			want:   assert.False,
 		},
 		{
 			name:   "with iteration count less than maximum",
-			fields: fields{iterated: 10},
+			fields: fields{maxIteratedCount: 20, iterated: 10},
 			want:   assert.False,
 		},
 		{
 			name:   "with iteration count greater than maximum",
-			fields: fields{iterated: 30},
+			fields: fields{maxIteratedCount: 20, iterated: 30},
 			want:   assert.True,
 		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
-			var counter counter // nolint: vetshadow
-			counter.iterated = data.fields.iterated
-			counter.expired = data.fields.expired
+			counter := counter{ // nolint: vetshadow
+				maxIteratedCount: data.fields.maxIteratedCount,
 
+				iterated: data.fields.iterated,
+			}
 			got := counter.stopIterate()
 
 			data.want(test, got)
