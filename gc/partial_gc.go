1df57bcedc3463b870fd9802d0f6ff8e630bfb90
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-20 23:00:00 +0300

    Fix the gc.PartialGC structure

--- gc/partial_gc.go
+++ gc/partial_gc.go
@@ -9,13 +9,15 @@ import (
 
 // PartialGC ...
 type PartialGC struct {
-	storage hashmap.Storage
-	clock   cache.Clock
+	storage           hashmap.Storage
+	clock             cache.Clock
+	maxIteratedCount  int
+	minExpiredPercent float64
 }
 
 // NewPartialGC ...
 func NewPartialGC(storage hashmap.Storage, clock cache.Clock) PartialGC {
-	return PartialGC{storage, clock}
+	return PartialGC{storage, clock, maxIteratedCount, minExpiredPercent}
 }
 
 // Clean ...
@@ -28,7 +30,8 @@ func (gc PartialGC) Clean(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		default:
-			iterator := newIterator(gc.storage, gc.clock)
+			iterator :=
+				newIterator(gc.storage, gc.clock, gc.maxIteratedCount, gc.minExpiredPercent)
 			gc.storage.Iterate(withInterruption(ctx, iterator.handleIteration))
 
 			if iterator.stopClean() {
