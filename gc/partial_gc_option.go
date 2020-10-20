3ccbdfbf89cfa7c56d29f6a3c792d6c3826c5e3d
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-10-20 23:00:00 +0300

    Add the gc.PartialGCWithClock() option

--- gc/partial_gc_option.go
+++ gc/partial_gc_option.go
@@ -1,4 +1,18 @@
 package gc
 
+import (
+	cache "github.com/thewizardplusplus/go-cache"
+)
+
 // PartialGCOption ...
 type PartialGCOption func(gc *PartialGC)
+
+// PartialGCWithClock ...
+//
+// Default: the time.Now() function.
+//
+func PartialGCWithClock(clock cache.Clock) PartialGCOption {
+	return func(gc *PartialGC) {
+		gc.clock = clock
+	}
+}
